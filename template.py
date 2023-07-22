67cac7f54425cd2105997d0e58e804638b7aa4f6
Sanjay D <118202636+Dr-Sanjay@users.noreply.github.com>
2023-07-22 13:44:20 +0530

    modified templplate

--- template.py
+++ template.py
@@ -42,4 +42,4 @@ for filepath in list_of_files:
             pass
             logging.info(f"Creating empty file:{filepath}")
     else:
-        logging,info(f"{filename} is already exists")
+        logging.info(f"{filename} is already exists")
